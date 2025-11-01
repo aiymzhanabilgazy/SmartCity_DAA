e992d788198b7cff221f856a37e0eb64c7d8f99e
Aiymzhan <aikogirl483@gmail.com>
2025-11-02 02:41:15 +0500

    tested the condensation graph

---
+++ src/test/java/CondensationGraphTest.java
@@ -0,0 +1,28 @@
+
+import graph.model.Graph;
+import graph.scc.CondensationGraph;
+import graph.scc.Kosaraju;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CondensationGraphTest {
+
+    @Test
+    public void testCondensationEdges() {
+        Graph g = new Graph(4, true);
+
+        g.addEdge(0, 1, 1);
+        g.addEdge(1, 0, 1);
+        g.addEdge(1, 2, 1);
+        g.addEdge(2, 3, 1);
+
+        Kosaraju.Result scc = new Kosaraju().findSCC(g);
+        Graph dag = CondensationGraph.buildCondensation(g, scc);
+
+        assertEquals(3, dag.getNodesCount());
+
+        int edgeCount = dag.getAdjList().values().stream().mapToInt(java.util.List::size).sum();
+        assertEquals(2, edgeCount);
+    }
+}
